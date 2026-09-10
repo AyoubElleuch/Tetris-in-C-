a309443d46b0e8b96a42ace3644a3ad023d4ae33
Mohamed Ayoub Eleuch <mmdayoub3@gmail.com>
2026-09-10 14:26:22 +0100

    Initialize SDL2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include<stdio.h>
 
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
 using namespace std;
 
 int main(){
+    const int initResult = SDL_Init(SDL_INIT_VIDEO);
+    if (initResult != 0) {
+        printf("SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
 
+
+    SDL_Quit();
     return 0;
 }
